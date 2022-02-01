14a4b5f377bb9163c04cc392400cb13e73493add
Eric van Fandenfart <ruhlanderic@gmail.com>
2022-02-01 23:19:35 +0100

    Initial Version

---
+++ TouchCamera/TouchButton.cs
@@ -0,0 +1,82 @@
+﻿using MelonLoader;
+using System;
+using UnhollowerBaseLib;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+
+namespace TouchCamera
+{
+
+    [RegisterTypeInIl2Cpp]
+    public class TouchButton : MonoBehaviour
+    {
+
+        //[method: HideFromIl2Cpp]
+        //public event Action? OnEnabled;
+
+        //[method: HideFromIl2Cpp]
+        //public event Action? OnDisabled;
+        private RectTransform rectTransform;
+        private Button button;
+        private Toggle toggle;
+        private Il2CppStructArray<Vector3> worldPosition;
+        static float lastInteraction = 0;
+        static bool lastPressed = false;
+        bool lastPressedLocal = false;
+        
+        public TouchButton(IntPtr obj0) : base(obj0)
+        {
+            rectTransform = gameObject.GetComponent<RectTransform>();
+            button = gameObject.GetComponent<Button>();
+            toggle = gameObject.GetComponent<Toggle>();
+            worldPosition = new Il2CppStructArray<Vector3>(4);
+        }
+
+        void Update()
+        {
+            if (lastInteraction + 1.5f > Time.time)
+                return;
+            Vector3? fingerPos = Networking.LocalPlayer?.GetBonePosition(HumanBodyBones.RightIndexDistal);
+            if (!fingerPos.HasValue)
+                return;
+
+            //Each corner provides its world space value. The returned array of 4 vertices is clockwise.
+            //It starts bottom left and rotates to top left, then top right, and finally bottom right.
+            //Note that bottom left, for example, is an (x, y, z) vector with x being left and y being bottom.
+            rectTransform.GetWorldCorners(worldPosition);
+
+            Vector3 pos1 = worldPosition[0] + (Vector3.Scale(rectTransform.forward, new Vector3(0.05f, 0.05f, 0.05f)));
+            Vector3 pos2 = worldPosition[2] - (Vector3.Scale(rectTransform.forward, new Vector3(0.05f, 0.05f, 0.05f)));
+
+            Vector3 min = Vector3.Min(pos1, pos2);
+            Vector3 max = Vector3.Max(pos1, pos2);
+
+            Vector3 fingerPosValue = fingerPos.Value;
+
+            if (fingerPosValue.x < max.x &&
+                fingerPosValue.y < max.y &&
+                fingerPosValue.z < max.z &&
+                fingerPosValue.x > min.x &&
+                fingerPosValue.y > min.y &&
+                fingerPosValue.z > min.z)
+            {
+                if (!lastPressed)
+                {
+                    lastPressed = true; 
+                    lastPressedLocal = true;
+                    button?.Press();
+                    toggle?.InternalToggle();
+                }
+            }
+            else if(lastPressedLocal)
+            {
+                lastPressedLocal = false;
+                lastPressed = false;
+                lastInteraction = Time.time;
+            }
+
+            
+        }
+    }
+}
---
+++ TouchCamera/TouchCameraMod.cs
@@ -0,0 +1,99 @@
+﻿using TouchCamera;
+using MelonLoader;
+using UnityEngine;
+using VRC.UserCamera;
+using System.Collections;
+using UnityEngine.UI;
+using UnhollowerBaseLib;
+using System.IO;
+
+[assembly: MelonInfo(typeof(TouchCameraMod), "TouchCamera", "1.0.0", "Eric van Fandenfart")]
+[assembly: MelonGame]
+
+namespace TouchCamera
+{
+
+    public class TouchCameraMod : MelonMod
+    {
+        
+
+        public override void OnApplicationStart()
+        {
+            MelonCoroutines.Start(WaitForCamera());
+        }
+
+        private IEnumerator WaitForCamera()
+        {
+            while (UserCameraController.field_Internal_Static_UserCameraController_0 == null)
+                yield return null;
+            var cameraobj = UserCameraController.field_Internal_Static_UserCameraController_0.transform;
+
+            while (cameraobj.Find("ViewFinder/PhotoControls/Primary /ControlGroup_Main/ControlGroup_Space/Scroll View/Viewport/Content/Attached/Icon")?.GetComponent<CanvasRenderer>()?.GetMaterial()?.shader == null)
+                yield return null;
+
+            var buttonParent = cameraobj.Find("ViewFinder/PhotoControls/Primary /ControlGroup_Main").gameObject;
+            LoggerInstance.Msg("Registering TouchButton");
+            Il2CppArrayBase<Button> allButtons = buttonParent.GetComponentsInChildren<Button>();
+            foreach (var item in allButtons)
+            {
+                item.gameObject.AddComponent<TouchButton>();
+
+            }
+            LoggerInstance.Msg("Registered TouchButton");
+
+            LoggerInstance.Msg("Disabling Overrender");
+
+            SetLayerRecursively(cameraobj.Find("ViewFinder/PhotoControls").gameObject, 3);
+            SetLayerRecursively(cameraobj.Find("ViewFinder/UserCamera_New").gameObject, 3);
+            AssetBundle bundle;
+
+            LoggerInstance.Msg("Loading replacment shaders from assetbundle");
+            using (var stream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("TouchCamera.shaderreplacment"))
+            using (var tempStream = new MemoryStream((int)stream.Length))
+            {
+                stream.CopyTo(tempStream);
+                bundle = AssetBundle.LoadFromMemory(tempStream.ToArray(), 0);
+            }
+
+            var uishader = bundle.LoadAsset<Shader>("Assets/UIReplacement.shader");
+            var uishaderTMPRO = bundle.LoadAsset<Shader>("Assets/TextMesh Pro/Shaders/TMP_SDF-Mobile-Replacment.shader");
+
+            LoggerInstance.Msg("Loading shaders");
+            LoggerInstance.Msg("Applying shaders");
+
+            ReplaceShader(cameraobj, "ViewFinder/PhotoControls/Primary /ControlGroup_Main/Scroll View/Viewport/Content/Space/Icon", uishader);
+            ReplaceShader(cameraobj, "ViewFinder/PhotoControls/Primary /ControlGroup_Main/ControlGroup_Space/Scroll View/Viewport/Content/Attached/Icon", uishader);
+            ReplaceShader(cameraobj, "ViewFinder/PhotoControls/Controls _ OnScreen/FrontCenter/Zoom _ Slider/Slider/Background/Fill Area", uishader);
+            ReplaceShader(cameraobj, "ViewFinder/PhotoControls/Controls _ OnScreen/FrontCenter/Zoom _ Slider/Slider/Background/Fill Area/Fill", uishader);
+            ReplaceShader(cameraobj, "ViewFinder/PhotoControls/Controls _ OnScreen/FrontCenter/Zoom _ Slider/Slider/Background/Fill Area", uishader);
+            ReplaceShader(cameraobj, "ViewFinder/PhotoControls/Primary /ControlGroup_Main/Scroll View/Scrollbar Horizontal/Sliding Area/Handle", uishader);
+            ReplaceShader(cameraobj, "ViewFinder/PhotoControls/Primary /ControlGroup_Main/RightArrow/Image", uishader);
+
+            ReplaceShader(cameraobj, "ViewFinder/PhotoControls/Primary /ControlGroup_Main/Scroll View/Viewport/Content/CameraAimMode/Text (TMP)", uishaderTMPRO);
+
+
+
+            while (cameraobj.Find("ViewFinder/PhotoControls/Primary /ControlGroup_Main/SelectedGroupHighlightArrow")?.GetComponent<CanvasRenderer>()?.GetMaterial()?.shader == null)
+                yield return null;
+
+            ReplaceShader(cameraobj, "ViewFinder/PhotoControls/Primary /ControlGroup_Main/SelectedGroupHighlightArrow", uishader);
+
+            LoggerInstance.Msg("Disabled Overrender");
+        }
+
+        private void ReplaceShader(Transform transform,string path, Shader shader)
+        {
+            transform.Find(path).GetComponent<CanvasRenderer>().GetMaterial().shader = shader;
+        }
+
+        private void SetLayerRecursively(GameObject obj,int newLayer)
+        {
+            obj.layer = newLayer;
+            for (int i = 0; i < obj.transform.childCount; i++)
+            {
+                SetLayerRecursively(obj.transform.GetChild(i).gameObject, newLayer);
+            }
+        }
+
+    }
+}
